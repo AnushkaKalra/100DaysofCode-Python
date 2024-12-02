2a1656272fc21bde5bf78e4c6fa67cc21fe47f5b
Anushka Kalra <kalraanushka@gmail.com>
2024-12-02 23:23:34 +0530

    Revert "day 5 completed"
    This reverts commit fb1bc42b1bb29d8f33e1e1e8ce7ff6f52ff501ab.

--- Day 5/For Loops with Range/task.py
+++
@@ -1,4 +0,0 @@
-total = 0
-for num in range(1,101):
-    total += num
-print(total)    # Total contains the sum of all the numbers between 1 and 100, inclusive of both 1 and 100
--- Day 5/Highest Score/task.py
+++ Day 5/Highest Score/task.py
@@ -1,6 +1,2 @@
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
-max_score = 0
-for i in range(len(student_scores)):
-    if student_scores[i] > max_score: # Iteration through the array
-        max_score = student_scores[i]
-print(max_score)
+print(range(1, 10))
