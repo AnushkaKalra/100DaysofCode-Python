fb1bc42b1bb29d8f33e1e1e8ce7ff6f52ff501ab
Anushka Kalra <kalraanushka@gmail.com>
2024-12-02 23:21:21 +0530

    day 5 completed

---
+++ Day 5/For Loops with Range/task.py
@@ -0,0 +1,4 @@
+total = 0
+for num in range(1,101):
+    total += num
+print(total)    # Total contains the sum of all the numbers between 1 and 100, inclusive of both 1 and 100
--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,4 +1,9 @@
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+# Password Generator Project
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -7,4 +12,48 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+# Easy Version - Order not randomised i.e. the password is in sequence
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
+
+# Hard Version -  Order of characters randomised i.e. the password is not in sequence
+# e.g. 4 letter, 2 symbol, 2 number = g^2jk8&
+
+level = input("Choose the level \'Easy\' or \'Hard\'? ")
+
+if level == 'Easy':
+    # Easy level
+    password = ""
+
+    for i in range(1, nr_letters + 1):
+        password += random.choice(letters)
+    for i in range(1, nr_symbols + 1):
+        password += random.choice(symbols)
+    for i in range(1, nr_numbers + 1):
+        password += random.choice(numbers)
+
+    print("The Password is ", password)
+
+elif level == 'Hard':
+    # Hard level
+    passwordList = []
+
+    for char in range(1, nr_letters + 1):
+        passwordList.append(random.choice(letters))
+    for char in range(1, nr_symbols + 1):
+        passwordList.append(random.choice(symbols))
+    for char in range(1, nr_numbers + 1):
+        passwordList.append(random.choice(numbers))
+
+    # print(passwordList) #before shuffling
+    random.shuffle(passwordList) #randomizes the order/sequence of characters in password
+    # print(passwordList) #after shuffling
+
+    password = ""
+    for ch in passwordList:
+        password += ch
+
+    print("The Password is", password)
+
+else:
+    print("Invalid level. Choose the level again.")
