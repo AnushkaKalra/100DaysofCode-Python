e8977feccdb08bc6a54f52083f6034cdeabca897
Anushka Kalra <kalraanushka@gmail.com>
2024-12-19 01:08:20 +0530

    second commit

---
+++ Newspaper 0 beginner code.py
@@ -0,0 +1,26 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+take()
+for i in range(3):
+    turn_left()
+    move()
+    turn_right()
+    move()
+    move()
+put()
+for i in range(2):
+    turn_left()
+for i in range(3):
+    move()
+    move()
+    turn_left()
+    move()
+    turn_right()
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
---
+++ Newspaper 0 intermediate code.py
@@ -0,0 +1,35 @@
+def turn_right():
+    for i in range(3):
+        turn_left()
+
+def turn_around():
+    for i in range(2):
+        turn_left()
+        
+def up_three_steps():
+    for step in range(3):
+        turn_left()
+        move()
+        turn_right()
+        for j in range(2):
+            move()
+            
+def down_three_steps():
+    for step in range(3):
+        for j in range(2):
+            move()
+        turn_left()
+        move()
+        turn_right()
+        
+# main
+take()
+up_three_steps()
+put()
+turn_around()
+down_three_steps()
+        
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
