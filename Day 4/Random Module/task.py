50f23ef4490f17058ba46238793472f0843a1619
Anushka Kalra <kalraanushka@gmail.com>
2025-01-10 23:09:39 +0530

    day 4 completed

--- Day 4/Random Module/task.py
+++ Day 4/Random Module/task.py
@@ -1,7 +1,23 @@
 import random
 
-random_number = random.randint(0,1)
-if random_number == 0:
+# Random Whole Numbers Within a Range
+print("Random Integer:",random.randint(1,100)) # both 1 and 100 are inclusive
+
+# Random Floats
+rand_num_0_to_1 = random.random() # will generate a random float between 0.0 (inclusive) and 1.0 (not inclusive)
+print("Random Float between 0 and 1:", rand_num_0_to_1)
+
+rand_num_0_to_10 = random.random() * 10 # this will expand the range between 0.0 <= random_number < 10.0
+print("Random Float between 0 and 10:", rand_num_0_to_10)
+
+random_float = random.uniform(1, 100) # Another way to generate random floating point numbers. May or may not include upper bound limit
+# In general, a <= random.uniform(a,b) <= b
+print("Random Float between 0 and 100:", random_float)
+
+print("HEADS OR TAILS")
+
+choice = random.randint(0,1)
+if choice == 0:
     print("Heads")
 else:
     print("Tails")
--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -33,22 +33,22 @@ options_string = ['Rock', 'Paper', 'Scissors']
 user_choice = int(input(f"Enter your choice: 0 for rock, 1 for paper, 2 for scissors? "))
 
 if user_choice in [0,1,2]:
-    print(f"The user chose: {options_string[user_choice]}")
+    print(f"The user chose: {options_string[user_choice]}.")
     print(options[user_choice])
 else:
-    print(f"Please choose a valid option.")
+    print("Please choose a valid option.")
 
 computer_choice = random.randint(0,2)
-print(f"The computer chose {options_string[computer_choice]}.")
+print(f"The computer chose: {options_string[computer_choice]}.")
 print(options[computer_choice])
 
 if user_choice == computer_choice:
-    print("It's a draw!")
-elif user_choice == 0 and options[computer_choice] == scissors:
+    print("It\'s a draw!")
+elif user_choice == 0 and computer_choice == 2:
     print("You won!")
-elif user_choice == 1 and options[computer_choice] == rock:
+elif user_choice == 1 and computer_choice == 0:
     print("You won!")
-elif user_choice == 2 and options[computer_choice] == paper:
+elif user_choice == 2 and computer_choice == 1:
     print("You won!")
 else:
     print("Sorry you lost!")
