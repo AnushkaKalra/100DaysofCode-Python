a1d2de170ee4c3020c190e41fcde48a981591e16
Anushka Kalra <kalraanushka@gmail.com>
2025-02-06 10:49:12 +0530

    day 13 completed

--- Day 13/Reproduce the Bug/task.py
+++ Day 13/Reproduce the Bug/task.py
@@ -1,4 +1,11 @@
 from random import randint
 dice_images = ["❶", "❷", "❸", "❹", "❺", "❻"]
 dice_num = randint(1, 6)
-print(dice_images[dice_num])
+print(dice_images[dice_num-1])
+
+# or
+
+#dice_images = ["❶", "❷", "❸", "❹", "❺", "❻"]
+#dice_num = randint(1, 5)
+#print(dice_images[dice_num])
+
